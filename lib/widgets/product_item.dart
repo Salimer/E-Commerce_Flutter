fd60a0268beccc9681f1501f47a42779c6336650
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-28 15:03:12 +0300

    Update product_item.dart

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,4 @@
-import 'package:e_commerce_flutter/data/fake.dart';
+import 'package:e_commerce_flutter/model/product.dart';
 import 'package:e_commerce_flutter/screens/product_details.dart';
 import 'package:flutter/material.dart';
 
