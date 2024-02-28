3c7d29c89232f653717d22e50ae556241ed0fd69
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-28 16:19:03 +0300

    Update product.dart

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -1,7 +1,7 @@
 class Product {
   final int id;
   final String title;
-  final String price;
+  final double price;
   final String category;
   final String description;
   final String image;
@@ -14,4 +14,25 @@ class Product {
     required this.description,
     required this.image,
   });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return switch (json) {
+      {
+        'id': int id,
+        'title': String title,
+        'price': double price,
+        'category': String category,
+        'description': String description,
+        'image': String image,
+      } => Product(
+        id: id,
+        title: title,
+        price: price,
+        category: category,
+        description: description,
+        image: image,
+      ), _ => throw const FormatException('Failed to load product.')
+    };
+  }
 }
+
