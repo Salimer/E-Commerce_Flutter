0f44f88a21d765e6f8c5162ae3f2a2739efad231
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-28 15:03:37 +0300

    Create product.dart

---
+++ lib/model/product.dart
@@ -0,0 +1,17 @@
+class Product {
+  final int id;
+  final String title;
+  final String price;
+  final String category;
+  final String description;
+  final String image;
+
+  const Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.category,
+    required this.description,
+    required this.image,
+  });
+}
