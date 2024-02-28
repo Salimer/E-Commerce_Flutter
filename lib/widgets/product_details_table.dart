dd5b5e7895f72d52879a4915c228e67bf1d4c0a9
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-28 15:04:00 +0300

    Update product_details_table.dart

--- lib/widgets/product_details_table.dart
+++ lib/widgets/product_details_table.dart
@@ -8,49 +8,148 @@ class ProductDetailsTable extends StatelessWidget {
     return Center(
       child: Padding(
         padding: EdgeInsets.all(20),
-        child: Table(
-          border: TableBorder.all(
-            color: Colors.grey,
-          ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TableRow(
+            Container(
+              child: Text('Image'),
+            ),
+            Table(
+              border: TableBorder.all(color: Colors.black),
               children: [
-                const TableCell(
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text('Image'),
-                  ),
-                ),
-                TableCell(
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Title'),
-                        Text('Description'),
-                        Text('Category'),
-                        Text('Price'),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          children: [
-                            IconButton(
-                              icon: const Icon(Icons.shopping_cart),
-                              onPressed: () {
-                                print('tapped');
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                TableRow(
+                  children: [
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Name:'),
+                      ),
                     ),
-                  ),
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Product1'),
+                      ),
+                    )
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Description:'),
+                      ),
+                    ),
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Product1'),
+                      ),
+                    )
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Category:'),
+                      ),
+                    ),
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Product1'),
+                      ),
+                    )
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Price:'),
+                      ),
+                    ),
+                    TableCell(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Product1'),
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
+    );
+  }
+}
+
+class TableView extends StatelessWidget {
+  const TableView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Table(
+      border: TableBorder.all(
+        color: Colors.grey,
+      ),
+      children: [
+        TableRow(
+          children: [
+            const TableCell(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('Image'),
+              ),
+            ),
+            TableCell(
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text('Title'),
+                    Text('Description'),
+                    Text('Category'),
+                    Text('Price'),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          icon: const Icon(Icons.shopping_cart),
+                          onPressed: () {
+                            print('tapped');
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
