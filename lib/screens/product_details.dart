484c26f465741d881bb4314506c5395b6de44f6e
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-28 12:17:29 +0300

    Create product_details.dart

---
+++ lib/screens/product_details.dart
@@ -0,0 +1,20 @@
+import 'package:e_commerce_flutter/widgets/product_details_table.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatelessWidget {
+  const ProductDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text('Product Details'),
+      ),
+      body: ProductDetailsTable(),
+    );
+  }
+}
