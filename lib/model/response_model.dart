cfd666e1ffb23c71265fbfad0f9e6a655a05dcba
Salimer <52242629+Salimer@users.noreply.github.com>
2024-02-29 13:28:31 +0300

    Create response_model.dart

---
+++ lib/model/response_model.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+
+class ResponseModel {
+  final bool success;
+  final dynamic data;
+  final String? error;
+  final int statusCode;
+
+  const ResponseModel({required this.success, required this.statusCode, this.data, this.error});
+
+  factory ResponseModel.fromResponse(Response response){
+    final body = jsonDecode(response.body);
+
+    return ResponseModel(success: response.statusCode == 200 , statusCode: response.statusCode,data: body, error: 'error');
+  }
+}
