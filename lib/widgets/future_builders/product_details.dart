ec0fb7e6dc680c6e568a7f2f1c7513d6af1f20e1
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 09:55:18 +0300

    Create product_details.dart

---
+++ lib/widgets/future_builders/product_details.dart
@@ -0,0 +1,38 @@
+import 'package:e_commerce_flutter/model/product.dart';
+import 'package:e_commerce_flutter/service/products.dart';
+import 'package:e_commerce_flutter/widgets/product_details_table.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetailsBuilder extends StatefulWidget {
+  const ProductDetailsBuilder({super.key});
+
+  @override
+  State<ProductDetailsBuilder> createState() => _ProductDetailsBuilderState();
+}
+
+class _ProductDetailsBuilderState extends State<ProductDetailsBuilder> {
+  late Future<Product> futureProduct;
+
+  @override
+  void initState() {
+    super.initState();
+    futureProduct = fetchProduct();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Product>(
+        future: futureProduct,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ProductDetailsTable(item: snapshot.data!);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          // By default, show a loading spinner.
+          return const Center(child: CircularProgressIndicator());
+        },
+      );
+  }
+}
