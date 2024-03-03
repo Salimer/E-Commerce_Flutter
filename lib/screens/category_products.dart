67872d1d8072d17b994877a74c8896b78125cc74
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 10:29:56 +0300

    Update category_products.dart

--- lib/screens/category_products.dart
+++ lib/screens/category_products.dart
@@ -1,23 +1,8 @@
-import 'package:e_commerce_flutter/model/product.dart';
-import 'package:e_commerce_flutter/service/products.dart';
 import 'package:e_commerce_flutter/widgets/future_builders/category_products.dart';
 import 'package:flutter/material.dart';
 
-class CategoryProducts extends StatefulWidget {
+class CategoryProducts extends StatelessWidget {
   const CategoryProducts({super.key});
-
-  @override
-  State<CategoryProducts> createState() => _CategoryProductsState();
-}
-
-class _CategoryProductsState extends State<CategoryProducts> {
-  late Future<List<Product>> futureCategoryProducts;
-
-  @override
-  void initState() {
-    super.initState();
-    futureCategoryProducts = fetchCategoryProducts();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +16,7 @@ class _CategoryProductsState extends State<CategoryProducts> {
         ),
         title: const Text('Category Products'),
       ),
-      body: const CategoryProductsBuilder()
+      body: const CategoryProductsBuilder(),
     );
   }
 }
-
