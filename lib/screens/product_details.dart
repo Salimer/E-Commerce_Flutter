0c5cdfe41f6b02182167ab790e65d779b48514a2
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 10:29:59 +0300

    Update product_details.dart

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -1,24 +1,8 @@
-import 'package:e_commerce_flutter/model/product.dart';
-import 'package:e_commerce_flutter/services/products.dart';
 import 'package:e_commerce_flutter/widgets/future_builders/product_details.dart';
-import 'package:e_commerce_flutter/widgets/product_details_table.dart';
 import 'package:flutter/material.dart';
 
-class ProductDetails extends StatefulWidget {
+class ProductDetails extends StatelessWidget {
   const ProductDetails({super.key});
-
-  @override
-  State<ProductDetails> createState() => _ProductDetailsState();
-}
-
-class _ProductDetailsState extends State<ProductDetails> {
-  late Future<Product> futureProduct;
-
-  @override
-  void initState() {
-    super.initState();
-    futureProduct = fetchProduct();
-  }
 
   @override
   Widget build(BuildContext context) {
