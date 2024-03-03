bbc93d28003e2dcbdf2f357fbdd84e8b4fdf91e8
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 11:48:24 +0300

    Update product_details.dart

--- lib/screens/product_details.dart
+++ lib/screens/product_details.dart
@@ -2,7 +2,8 @@ import 'package:e_commerce_flutter/widgets/future_builders/product_details.dart'
 import 'package:flutter/material.dart';
 
 class ProductDetails extends StatelessWidget {
-  const ProductDetails({super.key});
+  final int id;
+  const ProductDetails({super.key, required this.id});
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +15,8 @@ class ProductDetails extends StatelessWidget {
         ),
         title: const Text('Product Details'),
       ),
-      body: const ProductDetailsBuilder(),
+      body: Center(
+          child: SingleChildScrollView(child: ProductDetailsBuilder(id: id))),
     );
   }
 }
