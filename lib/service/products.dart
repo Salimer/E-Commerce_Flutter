ed82c2a5b394f7b8984d6812eb17889159d64df9
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 10:30:11 +0300

    Update products.dart

--- lib/service/products.dart
+++ lib/service/products.dart
@@ -1,13 +1,12 @@
-
-
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:e_commerce_flutter/model/product.dart';
 
 Future<Product> fetchProduct() async {
-  final response = await http.get(Uri.parse('https://fakestoreapi.com/products/1'));
+  final response =
+      await http.get(Uri.parse('https://fakestoreapi.com/products/1'));
 
-  if(response.statusCode == 200) {
+  if (response.statusCode == 200) {
     return Product.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
   } else {
     throw Exception('Failed to load album');
@@ -15,13 +14,32 @@ Future<Product> fetchProduct() async {
 }
 
 Future<List<Product>> fetchCategoryProducts() async {
-  final response = await http.get(Uri.parse('https://fakestoreapi.com/products/category/jewelery'));
+  final response = await http
+      .get(Uri.parse('https://fakestoreapi.com/products/category/jewelery'));
 
-  if(response.statusCode == 200) {
-    final List<dynamic> productListData = jsonDecode(response.body) as List<dynamic>;
+  if (response.statusCode == 200) {
+    final List<dynamic> productListData =
+        jsonDecode(response.body) as List<dynamic>;
 
-    return productListData.map((productData) => Product.fromJson(productData)).toList();
+    return productListData
+        .map((productData) => Product.fromJson(productData))
+        .toList();
   } else {
     throw Exception('Failed to load products');
+  }
+}
+
+Future<List<String>> fetchCategories() async {
+  final response = await http.get(
+    Uri.parse('https://fakestoreapi.com/products/categories'),
+  );
+
+  if (response.statusCode == 200) {
+    final List<dynamic> categoriesListData =
+        jsonDecode(response.body) as List<dynamic>;
+
+    return categoriesListData.cast<String>();
+  } else {
+    throw Exception('Failed to load categories');
   }
 }
