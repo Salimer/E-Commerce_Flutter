c8510d5e80a2c7dec402b06c65a9fe2edd0bdcfb
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-03 10:29:50 +0300

    Update response_model.dart

--- lib/model/response_model.dart
+++ lib/model/response_model.dart
@@ -8,11 +8,16 @@ class ResponseModel {
   final String? error;
   final int statusCode;
 
-  const ResponseModel({required this.success, required this.statusCode, this.data, this.error});
+  const ResponseModel(
+      {required this.success, required this.statusCode, this.data, this.error});
 
-  factory ResponseModel.fromResponse(Response response){
+  factory ResponseModel.fromResponse(Response response) {
     final body = jsonDecode(response.body);
 
-    return ResponseModel(success: response.statusCode == 200 , statusCode: response.statusCode,data: body, error: 'error');
+    return ResponseModel(
+        success: response.statusCode == 200,
+        statusCode: response.statusCode,
+        data: body,
+        error: 'error');
   }
 }
