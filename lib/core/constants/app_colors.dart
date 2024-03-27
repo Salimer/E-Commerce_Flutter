1b643973eaa2642593807fa5b0a94d79730aba21
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-27 15:42:41 +0300

    Create app_colors.dart

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color tappedCategory = Color(0xff3A2C27);
+const Color unTappedCategory = Color(0xffF3F3F3);
+const Color categoryIcon = Color(0xff9D9D9D);
