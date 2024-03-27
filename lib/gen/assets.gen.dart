debd9c34d0451ade1ef305633347be1cf89492be
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-27 15:42:19 +0300

    Create assets.gen.dart

---
+++ lib/gen/assets.gen.dart
@@ -0,0 +1,30 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class $AssetsIconsGen {
+  const $AssetsIconsGen();
+
+  /// File path: assets/icons/Bell_pin.svg
+  String get bellPin => 'assets/icons/Bell_pin.svg';
+
+  /// File path: assets/icons/drawerIcon.svg
+  String get drawerIcon => 'assets/icons/drawerIcon.svg';
+
+  /// File path: assets/icons/notificationsIcon.svg
+  String get notificationsIcon => 'assets/icons/notificationsIcon.svg';
+
+  /// List of all assets
+  List<String> get values => [bellPin, drawerIcon, notificationsIcon];
+}
+
+class Assets {
+  Assets._();
+
+  static const $AssetsIconsGen icons = $AssetsIconsGen();
+}
