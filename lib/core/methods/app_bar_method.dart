67a6d1ab104003fbe349d7d57b4904f6bca0817e
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-28 16:39:40 +0300

    Update app_bar_method.dart

--- lib/core/methods/app_bar_method.dart
+++ lib/core/methods/app_bar_method.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce_flutter/core/config/config.dart';
+import 'package:e_commerce_flutter/core/constants/constants.dart';
 import 'package:e_commerce_flutter/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
@@ -8,7 +9,8 @@ AppBar appBarMethod() {
     leading: Builder(
       builder: (context) => IconButton(
         onPressed: () => Scaffold.of(context).openDrawer(),
-        icon: SvgPicture.asset(Assets.icons.drawerIcon),
+        icon: SvgPicture.asset(Assets.icons.drawerIcon,
+        colorFilter: ColorFilter.mode(deepBrown, BlendMode.srcIn),),
       ),
     ),
     title: Text("Salim's Store", style: TextStyles.bold20),
@@ -17,7 +19,8 @@ AppBar appBarMethod() {
         onPressed: () {},
         icon: SvgPicture.asset(
           Assets.icons.notificationsIcon,
-          // colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          
+          colorFilter: ColorFilter.mode(deepBrown, BlendMode.srcIn),
           width: 30,
         ),
       )
