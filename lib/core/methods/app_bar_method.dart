4fbb5b834c7f6e7ca975a142aa8fef62b7572b04
Salimer <52242629+Salimer@users.noreply.github.com>
2024-03-28 13:29:56 +0300

    Update app_bar_method.dart

--- lib/core/methods/app_bar_method.dart
+++ lib/core/methods/app_bar_method.dart
@@ -4,21 +4,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 AppBar appBarMethod() {
-    return AppBar(
-      leading: Builder(
-          builder: (context) => IconButton(
-              onPressed: () => Scaffold.of(context).openDrawer(),
-              icon: SvgPicture.asset(Assets.icons.drawerIcon))),
-      title: Text("Salim's Store", style: TextStyles.bold20),
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: SvgPicture.asset(
-            Assets.icons.bellPin,
-            // colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
-            width: 35,
-          ),
-        )
-      ],
-    );
-  }
+  return AppBar(
+    leading: Builder(
+      builder: (context) => IconButton(
+        onPressed: () => Scaffold.of(context).openDrawer(),
+        icon: SvgPicture.asset(Assets.icons.drawerIcon),
+      ),
+    ),
+    title: Text("Salim's Store", style: TextStyles.bold20),
+    actions: [
+      IconButton(
+        onPressed: () {},
+        icon: SvgPicture.asset(
+          Assets.icons.notificationsIcon,
+          // colorFilter: ColorFilter.mode(Colors.black, BlendMode.srcIn),
+          width: 30,
+        ),
+      )
+    ],
+  );
+}
